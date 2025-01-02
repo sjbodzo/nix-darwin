3b3282e5bef3d71e22e8d3fbed3908c93118a4e0
Jess Bodzo <jess.bodzo@ditto.live>
2025-01-02 10:31:31 -0500

    add lua lang server

--- modules/apps.nix
+++ modules/apps.nix
@@ -20,7 +20,7 @@
     taps = [ ];
 
     # brew install
-    brews = [ "openconnect" "luarocks" ];
+    brews = [ "openconnect" "luarocks" "lua-language-server" ];
 
     # brew install --cask
     # these need to be updated manually
