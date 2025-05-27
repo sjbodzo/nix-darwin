0679e9916dfa5ba3882cf60c7558234db2021eaa
Jess Bodzo <jess.bodzo@ditto.live>
2025-05-27 13:46:45 -0400

    add jj and cargo binstall

--- modules/apps.nix
+++ modules/apps.nix
@@ -20,7 +20,7 @@
     taps = [ ];
 
     # brew install
-    brews = [ "openconnect" "luarocks" "lua-language-server" "sqlcmd" "cargo-nextest" "helm" "sshpass" "semgrep"];
+    brews = [ "openconnect" "luarocks" "lua-language-server" "sqlcmd" "cargo-nextest" "helm" "sshpass" "semgrep" "cargo-binstall" "jj" ];
 
     # brew install --cask
     # these need to be updated manually
