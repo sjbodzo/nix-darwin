9f208d4cc5a417dc169130ffc1dd50de9fe97b86
Jess Bodzo <jess.bodzo@ditto.com>
2025-06-17 11:49:37 -0400

    Add argocd cli

--- modules/apps.nix
+++ modules/apps.nix
@@ -20,7 +20,7 @@
     taps = [ "defenseunicorns/tap" ];
 
     # brew install
-    brews = [ "openconnect" "luarocks" "lua-language-server" "sqlcmd" "cargo-nextest" "helm" "sshpass" "semgrep" "cargo-binstall" "jj" "zarf" "uds" "azure-cli" ];
+    brews = [ "openconnect" "luarocks" "lua-language-server" "sqlcmd" "cargo-nextest" "helm" "sshpass" "semgrep" "cargo-binstall" "jj" "zarf" "uds" "azure-cli" "argocd" ];
 
     # brew install --cask
     # these need to be updated manually
